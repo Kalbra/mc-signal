c105d8af88ad29cfbd6554c3800ca3475f8c5853
Kalle Bracht <kalle.bracht@outlook.de>
2021-01-22 18:07:05 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mypythonlib',
+    name='mc-signal',
     packages=find_packages(),
     version='0.1.0',
-    description='My first Python library',
+    description='Connect files in python. The easiest event system.',
     author='Kalle Bracht',
     license='MIT',
-    url='https://github.com/Kalbra/mc-signal'
+    url='https://github.com/Kalbra/mc-signal',
 )
